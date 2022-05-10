dd16b5e1f985e55cb32d094b70ae1203c68d0efe
Juan Solorzano <jgsolorzano@gmail.com>
2022-05-09 22:44:35 -0400

    primer crud

---
+++ MyNewShop/MyNewShop/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyNewShop.Data.Entities;
+
+namespace MyNewShop.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
---
+++ MyNewShop/MyNewShop/Data/Entities/Country.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyNewShop.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name ="País")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage ="El campo {0} es obligatorio.")]
+        public string Name { get; set; }
+
+
+    }
+}
