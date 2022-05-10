8cefc91bbb7de992016e93f33646fec19cdeb932
Juan Solorzano <jgsolorzano@gmail.com>
2022-05-10 08:22:21 -0400

    CRUD sin asistentes (categorias)

--- MyNewShop/MyNewShop/Data/DataContext.cs
+++ MyNewShop/MyNewShop/Data/DataContext.cs
@@ -10,12 +10,14 @@ namespace MyNewShop.Data
 
         }
 
-        public DbSet<Country> Countries { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Country> Countries { get; set; }        
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+            modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();            
         }
     }
 }
